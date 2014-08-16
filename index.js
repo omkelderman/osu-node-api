9f149361c2599cc90e3efd635e97f5ec42e10862
Olle Kelderman <omkelderman@gmail.com>
2014-08-17 01:02:24 +0200

    did some things

--- index.js
+++ index.js
@@ -1,5 +1,50 @@
-module.exports = OsuApi;
+var request = require('request');
+
+exports.Api = OsuApi;
 
 function OsuApi(apiKey) {
     this.apiKey = apiKey;
 }
+
+OsuApi.prototype.callApi = (function(endpoint, obj, callback) {
+    obj.k = this.apiKey;
+    request.get({
+        url: 'https://osu.ppy.sh/api/'+endpoint,
+        qs: obj
+    }, function(error, response, body) {
+        if(error) {
+            callback(error, null);
+        } else {
+            var err = null;
+            var obj = null;
+            if(response.statusCode == 200) {
+                try {
+                    obj = JSON.parse(body);
+                } catch(e) {
+                    err = new Error("Could not parse json");
+                }
+            } else {
+                err = new Error("Bad http-response");
+            }
+            callback(err, obj);
+        }
+    });
+});
+
+OsuApi.prototype.getBeatmap = (function(id, callback) {
+    this.callApi('get_beatmaps', {b: id}, function(err, obj) {
+        if(err) {
+            callback(err, null);
+        } else {
+            callback(null, obj[0]);
+        }
+    });
+});
+
+OsuApi.prototype.getBeatmapSet = (function(id, callback) {
+    this.callApi('get_beatmaps', {s: id}, callback);
+});
+
+OsuApi.prototype.getBeatmapsByUser = (function(id, callback) {
+    this.callApi('get_beatmaps', {u: id}, callback);
+});
